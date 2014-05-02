63287a014b245f9ad77ac08c762b0addc68c8a86
Zack Douglas <zdouglas@t-sciences.com>
2014-05-02 07:57:34 -0400

    Oops, forgot to get the name; added better greeting, intent, and removed old greeting

--- awesome.py
+++ awesome.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python2.7
+import fileinput
 
 print 'Hello world!'
 
-print 'It\'s nice to meet you!'
-
 print 'My name is Awesome; what\'s yours?'
+for line in fileinput.input():
+  print 'Nice to meet you %s' % line
+  break
+
+print 'I\'d like to play a game...'
