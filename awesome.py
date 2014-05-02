b268c0764e96f0d9af3551c73c4e9d2b9220da6a
Zack Douglas <zdouglas@t-sciences.com>
2014-05-02 08:03:57 -0400

    Get user name from stdin

--- awesome.py
+++ awesome.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python2.7
+import fileinput
 
 print 'Hello world!'
 
 print 'My name is Awesome; what\'s yours?'
+for line in fileinput.input():
+  print 'Nice to meet you %s' % line
+  break
