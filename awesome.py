bd2effcbfd50176bd1993f35683971dd8c13af6a
Zack Douglas <zdouglas@t-sciences.com>
2014-05-02 08:08:15 -0400

    Wrote our RNG game
    Uses awesome things like
     * class variables
     * ...

--- awesome.py
+++ awesome.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2.7
+import random
 import fileinput
 
 print 'Hello world!'
@@ -9,3 +10,12 @@ for line in fileinput.input():
   break
 
 print 'I\'d like to play a game...'
+MAX_ATTEMPTS = 1000
+MIN_INT = 0
+MAX_INT = 100
+PRB = 65
+attempts = MAX_ATTEMPTS
+while attempts and random.randint(MIN_INT, MAX_INT) < PRB:
+  attempts -= 1
+  print 'Doh, keep guessing!'
+print 'That was fun! Let\'s move on...'
